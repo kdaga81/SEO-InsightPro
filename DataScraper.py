18b1531061edec71c9c4dbd99447c0faf01f88b9
kdaga81 <keshavdaga81@gmail.com>
2023-10-21 17:15:28 -0700

    Initial Commit
    Contains code to pull data from website and clean it

---
+++ DataScraper.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from stopwords import custom_stopwords  # Import custom stop words
+
+# Get the URL from the user
+url = input("Enter the URL you want to scrape: ")
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content of the page using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extract the text content and remove HTML tags using regular expressions
+    text = re.sub(r'<[^>]*>', '', soup.get_text())
+
+    # Remove excess spaces by replacing multiple spaces with a single space
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    # Tokenize the text
+    words = text.split()
+
+    # Remove custom stop words
+    filtered_words = [word for word in words if word.lower() not in custom_stopwords]
+
+    # Join the filtered words back into a text
+    filtered_text = ' '.join(filtered_words)
+
+    # Print the cleaned text without custom stop words
+    print(filtered_text)
+else:
+    print("Failed to retrieve the web page. Status code:", response.status_code)
