153254d790ec8c02be83759189ef8596418d9fd5
Akash Butala <akash.butala.365@gmail.com>
2023-10-24 14:39:48 -0700

    Added cors for cross connection between react and flask

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import findWordCount
 import DataScraper
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 # yet to complete dont run this
 url = input("Enter the URL you want to scrape: ")
@@ -10,7 +12,7 @@ scrapped_data = DataScraper.getWepageData(url)
 algorithmselected = input("NaiveStringMatching  KMPAlgorithm  RabinKarbAlgorithm")
 
 
-@app.route("/wordCount")
+@app.route("/")
 def sendProcessTimeAndWordCountand():
     wordCount, process_time = findWordCount.getProcessTimeAndWordCount(
         scrapped_data, algorithmselected
